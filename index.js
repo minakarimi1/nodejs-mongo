d1a916d9d55bca989372046cf300e0d50fcc20d5
minakarimi1 <minakarimi1@yahoo.com>
2023-10-11 10:51:37 +0330

    connect to mongodb and added  schema model

---
+++ index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+mongoose
+  .connect("mongodb://127.0.0.1:27017/myapp2")
+  .then(() => console.log("connected to database"))
+  .catch((error) => console.log("can not connected to database") + error);
+
+  //create Schema
+  const usersShema = mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String, require: true },
+    age: {type : Number},
+    data : {type: Date , default: Date.now() },
+  });
+  
+  //Creating a User class from the schema model
+    const Users =  mongoose.model('Users' , usersShema);
+  
+
+  
