c40e9e7d7c9945a801ebcdf705ae6f9b85b7eac9
mohammad hadi <147169201+m6336mohammad@users.noreply.github.com>
2023-10-11 14:59:10 +0330

    Env express add (#1)
    * added env and express
    
    * added env file and port=8000
    
    * addes and import dotenv, express

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+import "dotenv/config";
 import mongoose from "mongoose";
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
 mongoose
   .connect("mongodb://127.0.0.1:27017/myapp")
   .then(() => console.log("connected to database"))
   .catch((error) => console.log("can not connected to database") + error);
 
-  //create Schema
-  const usersShema = new mongoose.Schema({
-    firstName: { type: String },
-    lastName: { type: String, require: true },
-    age: {type : Number},
-    data : {type: Date , default: Date.now() },
-  });
-  
-  //Creating a User class from the schema model
-    const Users =  mongoose.model('Users' , usersShema);
-  
+//create Schema
+const usersShema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String, require: true },
+  age: { type: Number },
+  data: { type: Date, default: Date.now() },
+});
 
-  
+//Creating a User class from the schema model
+const Users = mongoose.model("Users", usersShema);
+
+
+
+
+app.listen(port,()=>{
+  console.log(`server run on port ${port}` );
+})
