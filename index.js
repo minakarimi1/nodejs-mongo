a82ffe6cd5d0ebcf90f5149775fe12abe929ab4d
minakarimi1 <minakarimi1@yahoo.com>
2023-10-12 20:44:04 +0330

    added put servece from update usear

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ import express from "express";
 import mongoose from "mongoose";
 import usersRoute from "./usersRoute.js";
 
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json())
-const port = process.env.PORT || 3000;
 
+
+
+//connect database
 mongoose
   .connect("mongodb://127.0.0.1:27017/myapp")
   .then(() => console.log("connected to database"))
   .catch((error) => console.log("can not connect to database", error));
 
-app.use("/api/user", usersRoute);
+app.use("/api/users", usersRoute);
 
 app.listen(port, () => {
   console.log(`server run on port ${port}`);
--- usersRoute.js
+++ usersRoute.js
@@ -3,6 +3,7 @@ import express from "express";
 
 const userRouter = express.Router();
 
+
 userRouter.get("/", async (req, res) => {
   try {
     const userList = await Users.find();
@@ -11,14 +12,16 @@ userRouter.get("/", async (req, res) => {
       message: "OK",
     });
     console.log(userList);
+
   } catch (error) {
-    res.status(500).json({
-      data: null,
-      message: "Error while fetching users",
-      error: error.message,
-    });
+    res.status(404).json({
+      data:null,
+      massage: "Error while fetching user"
+    })
+    console.log(error.message);
   }
 });
+
 //get user by Id example=> /api/user/12345
 userRouter.get("/:id", async (req, res) => {
   try {
@@ -31,12 +34,13 @@ userRouter.get("/:id", async (req, res) => {
       message: "OK",
     });
     console.log(user);
+
   } catch (error) {
-    res.status(500).json({
+    res.status(404).json({
       data: null,
       message: "Error while fetching user by ID",
-      error: error.message,
     });
+    console.log(error.message);
   }
 });
 
@@ -51,17 +55,37 @@ userRouter.post("/", async (req, res) => {
 
     await newUser.save();
     console.log(newUser);
-    res.status(201).json({
+    res.status(200).json({
       data: newUser,
       message: "User created successfully",
     });
   } catch (error) {
-    res.status(500).json({
+    res.status(404).json({
       data: null,
       message: "Error while creating user",
       error: error.message,
     });
   }
 });
+
+//put
+// json format => /api/user/65280cab92...  update parametrs {"firstName":"mohammad"...}
+userRouter.put("/:id", async (req, res) => {
+  let userUpdate = await Users.findByIdAndUpdate(req.params.id, {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    age: req.body.age,
+  });
+
+  if (!userUpdate) {
+    return res.status(404).json({
+      data: null,
+      message: "this user with the given id was not find",
+    });
+  }
+
+  res.status(200).json({ data: userUpdate, message: "ok" });
+});
+
 
 export default userRouter;
